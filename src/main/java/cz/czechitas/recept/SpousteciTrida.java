d1a7cbb6a83c539e22fc5fede396798debc754b4
Neniax <peti69501@gmail.com>
2023-02-09 20:53:46 +0100

    final version with comments :)

--- src/main/java/cz/czechitas/recept/SpousteciTrida.java
+++ src/main/java/cz/czechitas/recept/SpousteciTrida.java
@@ -40,7 +40,43 @@ public class SpousteciTrida {
         // Pouzivejte napovidani v editoru.
         // Vyskakuje samo nebo pomoci Ctrl+Mezernik
 
-        cervenaMiska.nalozSiJedenKus(vajicka);
+        for (int i = 0; i < 4; i++) {
+            cervenaMiska.nalozSiJedenKus(vajicka);      //1. Miska si 4x vloží vajíčko
+        }
+        cervenaMiska.nalozSiCelyObsah(pytlikCukru);     //2. Miska si přidá cukr
+        mixer.zamichej(cervenaMiska);                   //3. Mixér to zamíchá
+        cervenaMiska.nalozSiCelyObsah(maslo125g);       //4. Miska si přidá máslo
+        mixer.zamichej(cervenaMiska);                   //5. Mixér to znovu zamíchá
+
+
+        kuchynskaVaha.vynulujSeS(zlutaMiska);           //6. navážit 250g mouky, když máme 1kg pytlík
+        while (kuchynskaVaha.zjistiHmotnost(zlutaMiska) != 250) {   // tzn. přisypat, odsypat....
+            if (kuchynskaVaha.zjistiHmotnost(zlutaMiska) < 250) {
+                zlutaMiska.nalozSiTrochu(pytlikMouky);
+            } else {
+                zlutaMiska.vylozSiTrochu();
+            }
+        }
+
+        cervenaMiska.nalozSiObsahJineMisky(zlutaMiska);     //7. První miska si mouku přebere
+        cervenaMiska.nalozSiCelyObsah(prasekDoPeciva);      //8. První miska si vezme prášek do pečiva
+        mixer.zamichej(cervenaMiska);                       //9. Mixér to zamíchá
+        plech.preberSiObsah(cervenaMiska);                  //10. Plech si přebere těsto z misky
+
+        for (int i = 0; i < 50; i++) {                      //11. Plech se 50x opakovaně posype kousky ovoce
+            plech.posypSeKusem(ovoce);
+        }
+
+        trouba.zapniSe(180);        //12. Trouba se zapne ...
+        trouba.nechejPect(5);          //12. ... a třeba 5 minut bude péct naprázdno
+        trouba.vlozSiDovnitr(plech);            //13. Trouba si vloží plech
+        trouba.nechejPect(25);         //14. Trouba ho bude péct 25 minut
+        trouba.vypniSe();                       //15. Trouba se zastaví
+        trouba.vyndejObsahVen();                //16.  Trouba vyndá obsah ven a je hotovo :-)
+
     }
 
 }
+
+
+
